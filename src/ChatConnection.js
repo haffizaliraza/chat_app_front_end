729feeee0374426fa8e36e3392060801d4f7f3b4
Ali Raza <haffizaliraza@gmail.com>
2022-03-01 14:23:47 +0500

    chat board update

---
+++ src/ChatConnection.js
@@ -0,0 +1,64 @@
+import ActionCable from 'actioncable'
+
+import {
+  V2_API_BASE_URL,
+  ACCESS_TOKEN_NAME,
+  CLIENT_NAME
+} from './globals.js'
+
+function ChatConnection(senderId, callback) {
+  let access_token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxfQ.JC6qKuH9SG0SIiYSfhZUFTtirxN9Q47buLk0DPFFFzE"
+  let client = "ali@gmail.com"
+
+  var wsUrl = 'ws://' + V2_API_BASE_URL + '/cable'
+  wsUrl += '?access-token=' + access_token + '&client=' + client
+
+  this.senderId = senderId
+  this.callback = callback
+
+  this.connection = ActionCable.createConsumer(wsUrl)
+  this.roomConnections = []
+}
+
+ChatConnection.prototype.talk = function(message, roomId) {
+  let roomConnObj = this.roomConnections.find(conn => conn.roomId == roomId)
+  if (roomConnObj) {
+    roomConnObj.conn.speak(message)
+  } else {
+    console.log('Error: Cannot find room connection')
+  }
+}
+
+ChatConnection.prototype.openNewRoom = function(roomId) {
+  if (roomId !== undefined) {
+    this.roomConnections.push({roomId: roomId, conn: this.createRoomConnection(roomId)})
+  }
+}
+
+ChatConnection.prototype.disconnect = function() {
+  this.roomConnections.forEach(c => c.conn.consumer.connection.close())
+}
+
+ChatConnection.prototype.createRoomConnection = function(room_code) {
+  var scope = this
+  return this.connection.subscriptions.create({channel: 'RoomChannel', room_id: room_code, sender: scope.senderId}, {
+    connected: function() {
+      console.log('connected to RoomChannel. Room code: ' + room_code + '.')
+    },
+    disconnected: function() {},
+    received: function(data) {
+      if (data.participants.indexOf(scope.senderId) != -1) {
+        return scope.callback(data)
+      }
+    },
+    speak: function(message) {
+      return this.perform('speak', {
+        room_id: room_code,
+        message: message,
+        sender:  scope.senderId
+      })
+    }
+  })
+}
+
+export default ChatConnection
---
+++ src/globals.js
@@ -0,0 +1,5 @@
+{
+    "V2_API_BASE_URL"= "localhost:3000",
+    "ACCESS_TOKEN_NAME"= "Authorization",
+    "CLIENT_NAME"= "client"
+}
